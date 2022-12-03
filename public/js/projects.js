1ad972b6e772b787abd5e7b6ce8de286a841b7d1
Meghsham15 <88832001+Meghsham15@users.noreply.github.com>
2022-12-04 00:21:16 +0530

    pop animation

--- public/js/projects.js
+++ public/js/projects.js
@@ -5,23 +5,20 @@ const proj = [
         img: "/img/hotel.png",
         name: "Hotel Website",
         des:"Responsive website for advertisement of a hotel situated near a beach",
-        link:"https://mj-hotel.azurewebsites.net/"
     },
     {
         id: 102,
         link: "https://obscure-springs-34432.herokuapp.com/",
         img: "/img/music.png",
         name: "Music Website",
-        des:"Responsive website for advertisement of a hotel situated near a beach",
-        link:"https://mj-hotel.azurewebsites.net/"
+        des:"In this project i have created music website which plays music produced by my friend.",
     },
     {
         id: 103,
         link: "https://fierce-inlet-60024.herokuapp.com/",
         img: "/img/Journal.png",
         name: "Journal Website",
-        des:"Responsive website for advertisement of a hotel situated near a beach",
-        link:"https://mj-hotel.azurewebsites.net/"
+        des:"Journal website with some basic mongo skills used in it",
     },
 ]
 
